62cb0c9a31ac4b01e2f1c39a849727de64b8b726
ColeDnought <97967701+ColeDnought@users.noreply.github.com>
2023-10-09 23:23:19 -0400

    Initial commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Form(isRegistered) {
+  if (isRegistered) {
+    return (
+      <form className="form">
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+    );
+  } else {
+    return (
+      <form className="form">
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <input type="password" placeholder="Confirm Password" />
+        <button type="submit">Register</button>
+      </form>
+    );
+  }
+}
+
+export default Form;
